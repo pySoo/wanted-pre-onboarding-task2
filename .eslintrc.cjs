1268382510022b908599d0c03d413e52b5dfb346
pySoo <soopy368@gmail.com>
2023-05-02 23:53:46 +0900

    [CHORE] : 프로젝트 초기 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh', '@typescript-eslint'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/interface-name-prefix': 'on',
+    '@typescript-eslint/explicit-function-return-type': 'on',
+    '@typescript-eslint/explicit-module-boundary-types': 'on',
+    '@typescript-eslint/no-explicit-any': 'on',
+  },
+};
