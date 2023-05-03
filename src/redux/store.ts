d30f2515e553be9a267e5527aaa2cae0e374cc78
pySoo <soopy368@gmail.com>
2023-05-04 04:58:19 +0900

    [FEAT] : 캐시 데이터 expire time 추가

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,9 @@
+import { setPurge } from './actions/searchAction';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore } from 'redux-persist';
 import persistReducer from '@/redux/reducers/persistReducer';
 import thunk from 'redux-thunk';
+import { EXPIRE_TIME } from '@/constants/config';
 
 declare global {
   interface Window {
@@ -13,5 +15,10 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(persistReducer, composeEnhancers(applyMiddleware(thunk)));
 const persistor = persistStore(store);
+
+setInterval(() => {
+  console.log('cache clear');
+  store.dispatch(setPurge());
+}, EXPIRE_TIME); // 테스트용으로 1분마다 초기화
 
 export { store, persistor };
